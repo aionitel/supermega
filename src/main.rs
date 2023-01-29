15a06617e82c54a583cdd038854132ec04eb3881
Alex Ionitel <oranggsoda@gmail.com>
2023-01-28 19:45:53 -0500

    keeping everything in one file for now, just parsing arguements and printing for now

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,38 @@
-mod art;
+use::colored::Colorize;
+use::clap::Parser;
+
+fn supermega() {
+    let logo1 = r#"    
+    /\  _`\ /\ \/\ \/\  _`\ /\  _`\ /\  _`\     
+    \ \,\L\_\ \ \ \ \ \ \L\ \ \ \L\_\ \ \L\ \   
+     \/_\__ \\ \ \ \ \ \ ,__/\ \  _\L\ \ ,  /   
+       /\ \L\ \ \ \_\ \ \ \/  \ \ \L\ \ \ \\ \  
+       \ `\____\ \_____\ \_\   \ \____/\ \_\ \_\
+        \/_____/\/_____/\/_/    \/___/  \/_/\/ /
+    "#;
+    let logo2 = r#"    
+    /'\_/`\/\  _`\ /\  _`\ /\  _  \            
+    /\      \ \ \L\_\ \ \L\_\ \ \L\ \           
+    \ \ \__\ \ \  _\L\ \ \L_L\ \  __ \          
+     \ \ \_/\ \ \ \L\ \ \ \/, \ \ \/\ \         
+      \ \_\\ \_\ \____/\ \____/\ \_\ \_\        
+       \/_/ \/_/\/___/  \/___/  \/_/\/_/    
+    "#;
+    print!("{}", logo1.bright_red());
+    println!("{}", logo2.bright_blue());
+}
+#[derive(Parser, Debug)]
+struct Cli {
+    // main category (e.g. "Justin")
+    #[arg(short, long)]
+    video: String,
+    
+    // number of videos to return
+    #[arg(short, long, default_value_t = 1)]
+    count: i32,
+}
 
 fn main() {
-    art::supermega();
+    let args = Cli::parse();
+    println!("Args: {:?}", args);
 }
