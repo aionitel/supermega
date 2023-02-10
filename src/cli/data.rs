7754a40574391a13bdb0f85aaa1fd5e30f1c07e3
Alex Ionitel <oranggsoda@gmail.com>
2023-02-10 01:18:14 -0500

    fetching youtube api working, printing request body for now

--- src/cli/data.rs
+++ src/cli/data.rs
@@ -1,4 +1,5 @@
 use std::env;
+use reqwest::Client;
 
 #[derive(Debug)]
 struct Video {
@@ -13,10 +14,15 @@ struct Video {
 // SuperMega youtube channel_id
 const channel_id: &str = "UCPPc2PdtA7gCMbjYp_i_TKA";
 
-pub async fn get_video() {
+pub async fn get_video() -> Result<(), reqwest::Error> {
+    // init reqwest client and get youtube api key from env
+    let client = Client::new();
     let api_key = env::var("API_KEY").expect("API_KEY must be set.");
 
     let url = format!("https://www.googleapis.com/youtube/v3/search?key={}&channelId={}&part=snippet&maxResults=10&q=plays", api_key, channel_id);
 
-    println!("{}", url);
+    let res = client.get(url).send().await.expect("failed to get response").json().await.expect("idk");
+    println!("{:?}", res);
+    
+    Ok(())
 }
